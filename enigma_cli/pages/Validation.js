fd2a9520441371cd7cef6cb1634b12cf1dd460f4
Otman-dev <mouhib_otm@upf.ac.ma>
2024-02-18 17:52:10 +0100

    Validation SUCCESS Run

--- enigma_cli/pages/Validation.js
+++ enigma_cli/pages/Validation.js
@@ -3,59 +3,68 @@ import Layout from './components/Layout';
 import axios from 'axios';
 
 const Validation = () => {
-  const [selectedModel, setSelectedModel] = useState(null);
-  const [selectedDataset, setSelectedDataset] = useState(null);
-  const [threshold, setThreshold] = useState(0.5);
+  const [selectedTraining, setSelectedTraining] = useState('');
+  const [selectedWeight, setSelectedWeight] = useState('');
   const [datasets, setDatasets] = useState([]);
-  const models = ['yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt', 'yolov8l.pt', 'yolov8x.pt'];
+  const [models, setModels] = useState([]);
+  const [weights, setWeights] = useState([]);
 
   useEffect(() => {
-    // Fetch datasets when the component mounts
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5001/get_datasets');
-        setDatasets(response.data.datasets);
+        const trainedModelsResponse = await axios.get('http://localhost:5001/get_T-Models');
+        console.log('Trained Models Response:', trainedModelsResponse.data);
+
+        setModels(trainedModelsResponse.data.datasets);
       } catch (error) {
-        console.error('Error fetching datasets:', error);
+        console.error('Error fetching trained models:', error);
       }
     };
 
     fetchData();
   }, []);
 
-  const handleSelectModel = (model) => {
-    setSelectedModel(model);
-    // Optionally, set the default selected dataset for the chosen model
-    setSelectedDataset(datasets[0]); // For example, selecting the first dataset by default
+  useEffect(() => {
+    const fetchWeights = async () => {
+      try {
+        if (selectedTraining) {
+          const weightsResponse = await axios.get(`http://localhost:5001/get_weights/${selectedTraining}`);
+          console.log('Weights Response:', weightsResponse.data);
+
+          setWeights(weightsResponse.data.weights);
+        }
+      } catch (error) {
+        console.error('Error fetching weights:', error);
+      }
+    };
+
+    fetchWeights();
+  }, [selectedTraining]);
+
+  const handleSelectTraining = (training) => {
+    setSelectedTraining(training);
   };
 
-  const handleSelectDataset = (dataset) => {
-    setSelectedDataset(dataset);
+  const handleSelectWeight = (weight) => {
+    setSelectedWeight(weight);
   };
 
-  const handleRunValidation = async () => {
+  const handleRunValidation = async (e) => {
+    e.preventDefault();
+
     try {
-      // Verification checks
-      if (!selectedModel || !selectedDataset || !threshold) {
+      if (!selectedTraining || !selectedWeight) {
         alert('Please fill in all the required fields');
         return;
       }
 
-      // Convert 'threshold' to float
-      const thresholdFloat = parseFloat(threshold);
-      const scriptPath = 'C:\\Users\\marom\\Desktop\\Enigma\\EnigmaAI\\src\\Utilities\\runValidation.py';
-
-      // Make a POST request to the server-side script
       const response = await axios.post('http://localhost:5001/run_python_script', {
-        script_path: scriptPath,
-        args: [selectedModel, selectedDataset, thresholdFloat],
+        script_path: 'C:\\Users\\pc\\Github\\EnigmaAI\\enigma_serversrc\\Utilities\\modelValid.py',
+        args: [selectedTraining, selectedWeight],
       });
 
       console.log(response.data);
-
-      // Handle the response from the server-side script as needed
     } catch (error) {
-      // Handle errors
       console.error('Error:', error);
     }
   };
@@ -67,12 +76,11 @@ const Validation = () => {
           <form onSubmit={handleRunValidation} className="space-y-4">
             <h1 className="text-2xl font-semibold text-center text-gray-800">Model Validation</h1>
 
-            {/* Select Model Dropdown */}
             <div>
-              <label className="block text-sm font-medium text-gray-700">Select Model:</label>
+              <label className="block text-sm font-medium text-gray-700">Select Training:</label>
               <select
-                value={selectedModel}
-                onChange={(e) => handleSelectModel(e.target.value)}
+                value={selectedTraining}
+                onChange={(e) => handleSelectTraining(e.target.value)}
                 className="mt-1 p-2 w-full border rounded-md"
               >
                 {models.map((model) => (
@@ -83,42 +91,25 @@ const Validation = () => {
               </select>
             </div>
 
-            {/* Display Selected Model */}
-            {selectedModel && <p className="text-lg mb-2">Selected Model: {selectedModel}</p>}
+            {selectedTraining && <p className="text-lg mb-2">Selected Training: {selectedTraining}</p>}
 
-            {/* Select Dataset Buttons */}
             <div>
-              <label className="block text-sm font-medium text-gray-700">Select Dataset:</label>
-              <div className="flex space-x-2">
-                {datasets.map((dataset, index) => (
-                  <button
-                    key={index}
-                    className={`px-4 py-2 rounded-md ${
-                      selectedDataset === dataset
-                        ? 'bg-primary text-white border border-primary'
-                        : 'bg-gray-200 text-gray-700'
-                    }`}
-                    onClick={() => handleSelectDataset(dataset)}
-                  >
-                    {dataset}
-                  </button>
-                ))}
-              </div>
-            </div>
-
-            {/* Validation Threshold */}
-            <div>
-              <label className="block text-sm font-medium text-gray-700">Validation Threshold:</label>
-              <input
-                type="number"
-                value={threshold}
-                onChange={(e) => setThreshold(e.target.value)}
-                step="0.01"
+              <label className="block text-sm font-medium text-gray-700">Select Weight:</label>
+              <select
+                value={selectedWeight}
+                onChange={(e) => handleSelectWeight(e.target.value)}
                 className="mt-1 p-2 w-full border rounded-md"
-              />
+              >
+                {weights.map((weight) => (
+                  <option key={weight} value={weight}>
+                    {weight}
+                  </option>
+                ))}
+              </select>
             </div>
 
-            {/* Run Validation Button */}
+            {selectedWeight && <p className="text-lg mb-2">Selected Weight: {selectedWeight}</p>}
+
             <button
               type="submit"
               className="w-full bg-primary text-white p-4 rounded-md hover:bg-opacity-80 focus:outline-none"
--- enigma_server/server.py
+++ enigma_server/server.py
@@ -3,12 +3,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from src.Utilities.roboFlowDataSet import roboflow_dataset
 from src.Utilities.modelTrain import m_train  # Import your training function
+from src.Utilities.modelValid import m_valid
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Assume datasets are located in the 'src/datasets' directory
 dataset_path = os.path.join(os.getcwd(), 'src', 'datasets')
+TmodelsPath = os.path.join(os.getcwd(), 'Train')
 
 @app.route('/run_python_script', methods=['POST'])
 def run_python_script():
@@ -24,6 +25,8 @@ def run_python_script():
             roboflow_dataset(*args)
         elif script_path.endswith('modelTrain.py'):
             m_train(*args)
+        elif script_path.endswith('modelValid.py'):
+            m_valid(*args)
         else:
             return jsonify({"result": None, "error": "Invalid script path"}), 400
 
@@ -36,6 +39,23 @@ def get_datasets():
     try:
         datasets = os.listdir(dataset_path)
         return jsonify({"datasets": datasets})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get_T-Models', methods=['GET'])
+def get_Trained_models():
+    try:
+        Tmodels = os.listdir(TmodelsPath)
+        return jsonify({"datasets": Tmodels})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/get_weights/<training>', methods=['GET'])
+def get_weights(training):
+    try:
+        weights_path = os.path.join(TmodelsPath, training, 'weights')
+        weights = os.listdir(weights_path)
+        return jsonify({"weights": weights})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
